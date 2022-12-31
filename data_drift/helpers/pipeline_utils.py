031ff7f73182f1f2dc9766e46b442bdbed90a00b
lleviraz <18896799+lleviraz@users.noreply.github.com>
2022-12-31 20:28:30 +0200

    some pipeline utils helpers

--- data_drift/helpers/pipeline_utils.py
+++ data_drift/helpers/pipeline_utils.py
@@ -1,11 +1,22 @@
-import json,os
+# generic 
+import os
+import sys 
+import json
+import numpy as np
+import pandas as pd
+from tqdm.notebook import tqdm
+from xgboost import XGBRegressor
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import get_cmap
+from sklearn.model_selection import train_test_split
 
-# data drift imports
-from data_helper import sample_from_data
-from drift_detection.drift_detector import DriftDetector
-from helpers.utils import calc_perf_kpis, recurring_val_in_lists
-from drift_detection.drift_testers.mmd_drift_tester import MMDDriftTester
-from drift_detection.drift_testers.pca_ks_drift_tester import PcaKsDriftTester
+# data drift specific
+# from boston_ds import BostonDS
+from utils import calc_perf_kpis
+from model_helper import XgbModel
+from helpers.data_helper import sample_from_data,change_int_values
+from ..drift_detection.drift_detector import DriftDetector
+from ..drift_detection.drift_testers.ks_drift_tester import KsDriftTester
 
 def load_configuration(config_file='config.json',verbose=False):
     '''
@@ -17,36 +28,129 @@ def load_configuration(config_file='config.json',verbose=False):
         config = json.load(json_file)
     
         print("Loading configuration from:",config_file )
-        print("*"*70)
+        print("*"*80)
     
         if(verbose):
             for k,v in config.items():
-                print('\tKey={},Value={}'.format(k,v))
-        print("*"*70)
+                print('Key={},Value={}'.format(k,v))
+        print("*"*80)
     return config
 
 
-# Test
-# boston_config_dict = load_configuration(config_file='../examples/boston_config.json',verbose=True)
+# Initial baseline predction / drift test plan
+def get_model_kpi(model,drift_detector,X_test,y_test):
+  '''
+  Initial baseline predction / drift test plan
+  '''
+  y_pred = model.predict(X_test)
+  kpi_sample = calc_perf_kpis(X_test, y_test, y_pred)
+  return kpi_sample
+
+def init_result_dfs(X_test,y_test,model,drift_detector):
+  '''
+  Calc and store initial model performance KPIs on test
+  '''
+  drift_test_results = drift_detector.test_drift(X_test)
+  y_pred = model.predict(X_test)
+  kpi = calc_perf_kpis(X_test, y_test, y_pred)
+
+  kpi['drift_detected'] = drift_test_results['drift_detected']
+  kpi['test_exceptions'] = drift_test_results['test_exceptions']
+  kpi_keys = [k for k in kpi.keys()]
+  kpi_columns = kpi_keys.append('retrain')
+  kpi['retrain'] = False
+
+  perf_kpis_prod = pd.DataFrame(columns=kpi_columns).append(kpi, ignore_index=True)
+  perf_kpis_base = pd.DataFrame(columns=kpi_columns).append(kpi, ignore_index=True)
+
+  return perf_kpis_base,perf_kpis_prod
 
 
+def add_artificial_noise_to_data(X_sample,current_batch,x_cat_features,columns_to_modify=[],start_drift_at_batch=100,noise_factor=0.01):
+  '''
+  Adds some noise to the data for simulating feature drift
+  '''
+  # modify data batch to create feature drift
+  for c in columns_to_modify:
+    if c in x_cat_features:
+      X_sample = change_int_values(X_sample, c, 0, 1, noise_factor * (current_batch - start_drift_at_batch))
+    else:
+      X_sample[c] = X_sample[c] + 1.5* X[c].std()# * noise_factor * (current_batch - start_drift_at_batch)
+  return X_sample.copy()
+
+def show_drift_detection_step(perf_kpis_base,perf_kpis_prod,title=''):
+  fig, axs = plt.subplots(2,1,figsize=(30, 5))
+  base_y = perf_kpis_base['drift_detected']
+  base_x = range(len(base_y))
+  axs[0].step(base_x, base_y, '-r*', where='post',label='baseline')
+  
+  prod_y = perf_kpis_prod['drift_detected']
+  prod_x = range(len(prod_y))
+  axs[1].step(prod_x, prod_y, '-r*', where='post',label='production',color='navy')
+  
+  plt.xlabel('Batch number')
+  plt.ylabel('Drift Status')
+  axs[0].set_yticks([0,1])
+  axs[1].set_yticks([0,1])
+  axs[0].set_title('Baseline')
+  axs[1].set_title('Production')
+  plt.suptitle(title)
+  plt.show()
+
+def report_summary_kpi(selected_dataset,perf_kpis_base,perf_kpis_prod,last_k_batches=100):
+  '''
+  Reports the summary KPI 
+  '''
+  mean_base = perf_kpis_base.iloc[-last_k_batches]['RMSE'].mean()
+  mean_prod = perf_kpis_prod.iloc[-last_k_batches]['RMSE'].mean()
+  diff_base_prod = mean_base-mean_prod
+  print('Average RMSE on last 100 batches: Baseline:{:.4f} vs. Production:{:.4f}'.format(mean_base,mean_prod))
+  if(selected_dataset=='BOSTON'):
+    print('Production model is better by {:.4f} , a potential saving of {:.2f}$ for the house predictions company'.format(diff_base_prod,diff_base_prod*1000))
+  else:
+    print('Production model is better by {:.4f} , a potential saving of {:.2f} claims per customer'.format(diff_base_prod,diff_base_prod))
 
 
+def train_fresh_models(selected_dataset,X_train,y_train):
+  '''
+  Fresh start for baseline and production models
+  (Returns two copies of the baseline model)
+  '''
+  baseline_model = XGBRegressor(objective='reg:squarederror')
+  baseline_model.fit(X_train, y_train)
 
-'''
-# TODO DELETE
-config_dict = {
-    'dataset_name': 'Boston',
-	'dataset_csv_path': 'datasets/boston_housing.csv',
-	'cat_features': [],
-    'int_features': ['CHAS', 'RAD', 'TAX'],
-    'cont_features': ['CRIM', 'ZN', 'INDUS', 'NOX', 'RM', 'AGE', 'DIS', 'PTRATIO', 'B', 'LSTAT'],
-    'target_label': 'PRICE'
-}
+  saved_model_filename = selected_dataset + "_baseline_model.json"
+  baseline_model.save_model(saved_model_filename)
 
-print(config_dict)
-# with open("config.json", "w") as fp:
-#     json.dump(config_dict,fp) 
-assert(config_dict==config_dict2)
-print(config_dict==config_dict2)
-'''
+  # Production model (a copy of the baseline initially)
+  production_model = XGBRegressor(objective='reg:squarederror')
+  production_model.load_model(saved_model_filename)
+
+  return baseline_model,production_model
+
+def init_test(X_train,y_train,X_test,y_test,
+               x_cont_features,x_int_features,x_cat_features,
+               selected_dataset):
+  '''
+  Init all required instances for a fresh test:
+  Returns:
+  1. 2 Models: baseline and production
+  2. 2 drift detectors baseline and production
+  3. 2 dataframes to collect the results
+  '''
+
+  baseline_model,production_model = train_fresh_models(selected_dataset,X_train,y_train)
+
+  # Create Feature drift detector with all default testers
+  drift_detector = DriftDetector()
+  drift_detector.autoselect_testers(x_cont_features, x_int_features, x_cat_features)
+  drift_detector.fit(X_train)
+
+  #One dummy drift detector for baseline model
+  base_drift_detector = DriftDetector()
+  base_drift_detector.autoselect_testers(x_cont_features, x_int_features, x_cat_features)
+  base_drift_detector.fit(X_train)
+
+  perf_kpis_prod,perf_kpis_base = init_result_dfs(X_test,y_test,production_model,drift_detector)
+    
+  return drift_detector,perf_kpis_prod,base_drift_detector,perf_kpis_base,baseline_model,production_model
